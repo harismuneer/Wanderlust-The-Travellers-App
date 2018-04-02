9925e0cf8c36aaf8ec2c7fed79a69ea979c6e08d
Your Name <example@example.com>
2018-04-02 21:57:02 +0500

    initial commit

--- app/src/main/java/com/project/wanderlust/Contacts.java
+++ app/src/main/java/com/project/wanderlust/Contacts.java
@@ -12,12 +12,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class Contacts extends AppCompatActivity implements RecyclerView.OnItemTouchListener{
 
-    public static ArrayList<String> names = new ArrayList<>();
-    public static ArrayList<String> phones = new ArrayList<>();
+//    public static ArrayList<String> names = new ArrayList<>();
+//    public static ArrayList<String> phones = new ArrayList<>();
 
     GestureDetector gestureDetector;
     Context c;
@@ -42,38 +49,67 @@ public class Contacts extends AppCompatActivity implements RecyclerView.OnItemTo
         });
 
         loadContactsFromPhone();
-
-        for(int i = 0; i < names.size(); i++) {
-            arrayList.add(new Contact(null, names.get(i), phones.get(i), "vfvd"));
-        }
-
-        adapter = new ContactAdapter(arrayList, R.layout.contact_cell);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        rv.addOnItemTouchListener(this);
-        rv.setItemAnimator(new DefaultItemAnimator());
-        rv.setAdapter(adapter);
     }
 
     private void loadContactsFromPhone() {
+        final ArrayList<String> names = new ArrayList<>();
+        final ArrayList<String> phones = new ArrayList<>();
         ContentResolver cr = getContentResolver();
         Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
         while (cursor.moveToNext()) {
             String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
 
             //  Get all phone numbers.
-            Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+            Cursor phones1 = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
 
-            while (phones.moveToNext()) {
-                String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)).replaceAll("\\s+","");
-                String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                if(!Contacts.phones.contains(number)) {
-                    Contacts.names.add(name);
-                    Contacts.phones.add(number);
+            while (phones1.moveToNext()) {
+                String number = phones1.getString(phones1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)).replaceAll("\\s+","");
+                String name = phones1.getString(phones1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                if(!phones.contains(number)) {
+                    names.add(name);
+                    phones.add(number);
                 }
             }
-            phones.close();
+            phones1.close();
         }
         cursor.close();
+
+        loadPeopleFromFirebase(names, phones);
+    }
+
+    private void loadPeopleFromFirebase(final ArrayList<String> names, final ArrayList<String> phones) {
+        final Context context = this;
+        arrayList.clear();
+
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i;
+                //going through all users present in firebase
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    String number = ds.getKey();
+                    String status = ds.child("status").getValue(String.class);
+                    //checking if obtaining user is not logged in user or phone contact
+                    if(!number.equals(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()) && phones.contains(number)) {
+                        if((i = phones.indexOf(number)) != -1) {
+                            arrayList.add(new Contact(null, names.get(i), status, number));
+                        }
+                    }
+                }
+                viewFriends();
+            }
+
+            @Override public void onCancelled(DatabaseError databaseError) { }
+        });
+    }
+
+    private void viewFriends() {
+        adapter = new ContactAdapter(arrayList, R.layout.contact_cell);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.addOnItemTouchListener(this);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setAdapter(adapter);
     }
 
     @Override
