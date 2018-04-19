4f08a37b185da1e3ec806f3918be7a0531832350
Your Name <example@example.com>
2018-04-20 00:10:41 +0500

    initial commit

--- app/src/main/java/com/project/wanderlust/CreateJourneyActivity.java
+++ app/src/main/java/com/project/wanderlust/CreateJourneyActivity.java
@@ -1,13 +1,93 @@
 package com.project.wanderlust;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class CreateJourneyActivity extends AppCompatActivity {
+    public static final int CAMERA = 2;
+    public static final int GALLERY = 3;
+
+    FirebaseUser mUser;
+    DatabaseReference mReference;
+
+    final ArrayList<Bitmap> photos = new ArrayList<>();
+    SelectedPicturesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_journey);
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mReference = FirebaseDatabase.getInstance().getReference("Journeys").child(mUser.getPhoneNumber());
+
+        adapter = new SelectedPicturesAdapter(this, photos);
+        GridView gridView = findViewById(R.id.photoGrid);
+        gridView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data != null) {
+            if (requestCode == CAMERA) {
+                photos.add((Bitmap) data.getExtras().get("data"));
+                adapter.notifyDataSetChanged();
+            }
+            else if (requestCode == GALLERY) {
+                if(data.getData() != null) {
+                    Uri mImageUri=data.getData();
+                    Bitmap bitmap = SharedFunctions.decodeBitmapFromFile(new File(mImageUri.toString()), 500, 500);
+                    photos.add(bitmap);
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    if(data.getClipData() != null){
+                        ClipData mClipData = data.getClipData();
+                        for(int i=0; i < mClipData.getItemCount(); i++){
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            Bitmap bitmap = SharedFunctions.decodeBitmapFromFile(new File(uri.toString()), 500, 500);
+                            photos.add(bitmap);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void createJourney(View view) {
+
+    }
+
+    public void Cancel(View view) { onBackPressed(); }
+
+    public void getCameraPicture(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA);
+    }
+
+    public void getGalleryPictures(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), GALLERY);
     }
 }
---
+++ app/src/main/java/com/project/wanderlust/SelectedPicturesAdapter.java
@@ -0,0 +1,44 @@
+package com.project.wanderlust;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class SelectedPicturesAdapter extends ArrayAdapter<Bitmap> {
+    private ArrayList<Bitmap> items;
+
+    public SelectedPicturesAdapter(Context context, ArrayList<Bitmap> items) {
+        super(context, 0, items);
+        this.items = items;
+    }
+
+    @Nullable
+    @Override
+    public Bitmap getItem(int position) {
+        return super.getItem(position);
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Bitmap bitmap = getItem(position);
+        ImageView imageView = new ImageView(getContext());
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageBitmap(bitmap);
+        convertView = imageView;
+        return convertView;
+    }
+}
